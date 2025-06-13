dcd375982ea8cca93c43fdeeaa038ea9367a215f
eve-66 <is0733pi@ed.ritsumei.ac.jp>
2025-06-13 23:59:41 +0900

    create button and state

---
+++ src/app/components/StateButtons.tsx
@@ -0,0 +1,34 @@
+'use client';
+import type { NextComponentType, NextPageContext } from "next";
+import { useState } from "react";
+
+const StateButtons: NextComponentType<NextPageContext> = () => {
+  const [state, setState] = useState<number>(0); // initial -> 0, start -> 1|3, stop -> 0, rest -> 2, restart -> 1|3
+
+  const handleClick = (nextState: number) => {
+    setState(nextState);
+  }
+
+  if(state === 0) {
+    return (
+      <div className='flex justify-center'>
+        <button className='bg-blue-500 text-2xl font-bold text-white px-4 py-2 rounded mx-4 my-2 hover:bg-blue-600' onClick={() => {handleClick(1)}}>Start</button>
+      </div>
+    )
+  } else if(state === 1 || state === 3) {
+    return (
+      <div className='flex justify-center'>
+        <button className='bg-red-500 text-2xl font-bold text-white px-4 py-2 rounded mx-4 my-2 hover:bg-red-600' onClick={() => handleClick(0)}>Stop</button>
+        <button className='bg-green-500 text-2xl font-bold text-white px-4 py-2 rounded mx-4 my-2 hover:bg-green-600' onClick={() => handleClick(2)}>Rest</button>
+      </div>
+    )
+  } else if(state === 2) {
+    return (
+      <div className='flex justify-center'>
+        <button className='bg-green-500 text-2xl font-bold text-white px-4 py-2 rounded mx-4 my-2 hover:bg-green-600' onClick={() => {handleClick(3)}}>Re Start</button>
+      </div>
+    )
+  }
+}
+
+export default StateButtons
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import StateButtons from "./components/StateButtons";
 import Time from "./components/Time";
 
 export default function Home() {
   return (
-    <div className="">
-      <Time />
+    <div className="bg-gray-200 my-20">
+      <div className="my-20">
+        <Time />
+      </div>
+      <div>
+        <StateButtons />
+      </div>
     </div>
   );
 }
